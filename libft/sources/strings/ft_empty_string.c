b14e9abe6da37e04bcc9e9fe7b0ff5b27be3dcb4
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2024-08-17 21:47:57 +0200

    malloc -> ft_calloc_if

--- libft/sources/strings/ft_empty_string.c
+++ libft/sources/strings/ft_empty_string.c
@@ -6,17 +6,18 @@
 /*   By: inikulin <inikulin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:01:40 by inikulin          #+#    #+#             */
-/*   Updated: 2023/11/22 20:28:11 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/08/17 21:45:31 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_empty_string(void)
 {
 	char	*res;
 
-	res = malloc(1);
+	res = ft_calloc_if(1, 1);
 	if (!res)
 		return (0);
 	res[0] = 0;
