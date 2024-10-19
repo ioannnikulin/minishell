d3b12bde7b5c0f99b2567c5a03ccf115ef9a2b3e
Tatiana <cr3mebrulee@proton.me>
2024-10-19 16:03:33 +0200

    fixup! compilation problem

--- sources/tree_creation/create_text_tree.c
+++ sources/tree_creation/create_text_tree.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 12:20:32 by taretiuk          #+#    #+#             */
-/*   Updated: 2024/10/19 15:56:22 by taretiuk         ###   ########.fr       */
+/*   Updated: 2024/10/19 16:03:04 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,5 @@
 int	create_text_tree(void)
 {
 	printf("create text tree\n");
-	// *tree = ft_tree_make();
-	// (*tree)->root = ft_treenode_make(input, 0, 0, ft_free_nop);
-	// if (!((*tree)->root))
-	// 	return (2);
-	// if (mock_0(input, (*tree)->root))
-	// 	return (0);
-	// printf("%s: %s\n", input, ERR_COMMAND_NOT_FOUND);
-	// return (3);
 	return (0);
 }
