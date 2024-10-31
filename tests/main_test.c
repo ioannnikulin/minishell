ae0ad92c0d9b8b2fea4eca441492b9d9218880d6
Tatiana <cr3mebrulee@proton.me>
2024-10-31 16:23:56 +0100

    fixup! fixup! fixup! fixup! fixup! refactoring parse_command

--- tests/main_test.c
+++ tests/main_test.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 00:03:03 by inikulin          #+#    #+#             */
-/*   Updated: 2024/10/23 19:27:45 by taretiuk         ###   ########.fr       */
+/*   Updated: 2024/10/31 16:23:36 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(void)
 {
-	// printf("input_to_text_tree in testing\n");
-	// input_to_text_tree_test();
+	printf("input_to_text_tree in testing\n");
+	input_to_text_tree_test();
 	printf("parsing in testing\n");
 	parse_command_test();
 	printf("\033[31mtesting complete\033[0m\n");
