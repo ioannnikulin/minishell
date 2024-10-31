20ace8c4a969a08dcbadd901e5e137b8aadee6d9
Tatiana <cr3mebrulee@proton.me>
2024-10-31 17:01:13 +0100

    norminette

--- libft/sources/map/mapss/ft_mapss_get.c
+++ libft/sources/map/mapss/ft_mapss_get.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_mapss_get.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 20:49:20 by inikulin          #+#    #+#             */
-/*   Updated: 2024/10/06 22:33:42 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/10/31 16:44:24 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_mapss_get(t_mapss *map, char *key)
 	if (!map || !map->head || !key)
 		return (0);
 	cur = map->head;
-	while (ft_strcmp(key, ((t_mapss_entry*)cur)->key) < 0)
+	while (ft_strcmp(key, ((t_mapss_entry *)cur)->key) < 0)
 		cur = cur->next;
-	if (ft_strcmp(key, ((t_mapss_entry*)cur)->key) == 0)
-		return (((t_mapss_entry*)cur)->value);
+	if (ft_strcmp(key, ((t_mapss_entry *)cur)->key) == 0)
+		return (((t_mapss_entry *)cur)->value);
 	return (0);
 }
