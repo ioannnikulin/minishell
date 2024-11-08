fa4963c8b5717789a19413870b0eff4ebecf289b
ioann <ioann.nikulin@gmail.com>
2024-11-08 12:39:21 +0100

    added tokenization testing into the proper file

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -6,7 +6,7 @@
 /*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 00:03:03 by inikulin          #+#    #+#             */
-/*   Updated: 2024/11/06 12:18:13 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/11/08 12:38:57 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ int	unit_tests(void)
 {
 	printf("input_to_text_tree in testing\n");
 	input_to_text_tree_test();
+	printf("tokenization in testing\n");
+	tokenize_cmd_test();
 	return (0);
 }
