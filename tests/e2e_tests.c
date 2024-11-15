a80c1fb7ecd7aa23700dd0e4eb263322c5ada077
ioann <ioann.nikulin@gmail.com>
2024-11-15 11:07:49 +0100

    debugging github

--- tests/e2e_tests.c
+++ tests/e2e_tests.c
@@ -6,7 +6,7 @@
 /*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 22:57:54 by inikulin          #+#    #+#             */
-/*   Updated: 2024/11/15 11:03:12 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/11/15 11:07:40 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,6 @@ static int	file_compare(char *exp_contens, char *act_fname)
 	int comp_res = regexec(&re, act, 0, NULL, 0);
 	ft_printf("comparison result %i, expected:[%s]\nactual:[%s]\nstrncmp %i %i\n", comp_res, exp_re, act, ft_strncmp(exp_contens, act, fsize - 1), ft_strncmp(exp_contens, act, fsize));
 	free(exp_re);
-	assert(comp_res == 0);
 	int i;
 	for (i = fsize - 2; act[i] >= '0' && act[i] <= '9'; i --);
 	char *sub = ft_substr(act, i, fsize - i);
