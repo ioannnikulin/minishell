6b517565f232d8da80d19dcf4c9bbaedc276caa8
Tatiana <cr3mebrulee@proton.me>
2024-11-26 10:30:51 +0100

    fixup! fixup! fixup! fixup! Merge branch 'dev' of github.com:ioannnikulin/minishell into 69_tree_mock_0

--- sources/input_to_text_tree_mocks/mock_16.c
+++ sources/input_to_text_tree_mocks/mock_16.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 23:07:09 by inikulin          #+#    #+#             */
-/*   Updated: 2024/11/24 11:53:54 by taretiuk         ###   ########.fr       */
+/*   Updated: 2024/11/26 10:28:54 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	mock_16_tree(t_treenode *cur)
 	cur = cur->child->sibling_next->sibling_next;
 	ft_treenode_insert_child_idx_s_dup(cur, "HOME", 0);
 	ft_treenode_insert_child_idx_s_dup(cur, "PATH", 1);
-	ft_treenode_insert_child_idx_s_dup(cur->parent, "pwd", 5);
 }
 
 /* returns 1 on success - to allow condition usage outside */
