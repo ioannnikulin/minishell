ad1d804b7f1a06ffbf87caa8383efe4ff88fb8de
Tatiana Retiukhina <taretiuk@student.42berlin.de>
2024-11-29 14:16:04 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Merge branch 'dev' of github.com:ioannnikulin/minishell into 69_tree_mock_0

--- tests/main_test.c
+++ tests/main_test.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 22:56:57 by inikulin          #+#    #+#             */
-/*   Updated: 2024/11/28 14:54:06 by taretiuk         ###   ########.fr       */
+/*   Updated: 2024/11/29 14:15:42 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int	main(void)
 {
 	ft_printf("unit tests in progress\n");
 	unit_tests();
-	// ft_printf("e2e tests in progress\n");
-	// e2e_tests();
-	// ft_printf("e2e tests completed\n");
-	// printf("tokenization in testing\n");
-	// tokenize_cmd_test();
+	ft_printf("e2e tests in progress\n");
+	e2e_tests();
+	ft_printf("e2e tests completed\n");
+	printf("tokenization in testing\n");
+	tokenize_cmd_test();
 	return (0);
 }
