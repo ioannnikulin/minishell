5a410230e207dccec63e7e66cfc97e30389cf633
Tatiana <cr3mebrulee@proton.me>
2024-12-02 20:49:53 +0100

    fixup! 22_mocks_passed_tests

--- tests/main_test.c
+++ tests/main_test.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 22:56:57 by inikulin          #+#    #+#             */
-/*   Updated: 2024/11/30 17:56:30 by taretiuk         ###   ########.fr       */
+/*   Updated: 2024/12/02 20:45:32 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	main(void)
 	ft_printf("unit tests in progress\n");
 	unit_tests();
 	ft_printf("unit tests completed\n");
-	// ft_printf("e2e tests in progress\n");
-	// e2e_tests();
-	// ft_printf("e2e tests completed\n");
+	ft_printf("e2e tests in progress\n");
+	e2e_tests();
+	ft_printf("e2e tests completed\n");
 	printf("tokenize_cmd in progress\n");
 	tokenize_cmd_test();
 	ft_printf("tokenize_cmd tests completed\n");
--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unit_tests.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 00:03:03 by inikulin          #+#    #+#             */
-/*   Updated: 2024/11/08 12:38:57 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/12/02 20:48:18 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	unit_tests(void)
 {
-	printf("input_to_text_tree in testing\n");
+	printf("input_to_text_tree in progress\n");
 	input_to_text_tree_test();
-	printf("tokenization in testing\n");
+	ft_printf("input_to_text_tree tests completed\n");
+	printf("tokenization in progress\n");
 	tokenize_cmd_test();
+	ft_printf("tokenization in progress tests completed\n");
 	return (0);
 }
