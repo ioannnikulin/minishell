11a4ae32a405f99b271bd246c25b98fe62ae1d29
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2024-12-13 17:31:23 +0100

    wip make control

--- sources/exec_text_tree.c
+++ sources/exec_text_tree.c
@@ -6,29 +6,46 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 13:39:01 by inikulin          #+#    #+#             */
-/*   Updated: 2024/12/13 12:43:08 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/12/13 17:25:50 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "tree_make/tree_processing_internal.h"
 
-/*
-static t_control	make_control_set_redirs(t_treenode *node)
+static t_control	*tofile(t_control *ctrl, char *fname, int append)
 {
-	t_control	res;
+	ctrl->out_fd = open(fname, O_WRONLY|O_CREAT|ft_if_i(append, O_APPEND, 0), 0600);
+	if (ctrl->out_fd == -1)
+	{
+		free(ctrl);
+		return (0);
+	}
+}
 
+t_control	*make_control_set_redirs(t_treenode *node)
+{
+	t_control	*res;
+	char		nodeptr[20];
+	int			ptrstart;
+
+	res = ft_calloc_if(sizeof(t_control), 1);
+	if (!res)
+		return (0);
 	res.retval = 0;
 	res.found = 0;
 	res.in_fd = -1;
 	res.out_fd = -1;
-	if (node->sibling_next && (!ft_strcmp(node->sibling_next->content, "|")
-		|| is_redirection(node->sibling_next->content)))
-	{
-		
-	}
+	if (!node->sibling_next)
+		return (res);
+	ptrstart = ft_ulltoa(node, nodeptr);
+	if (ft_strcmp(node->sibling_next->content, "|") == 0)
+		return (tofile(res, &nodeptr[ptrstart], 1));
+	else if (is_redirection(node->sibling_next->content))
+		return (tofile(res, node->sibling_next->sibling_next->content,
+				 ft_if_i(node->sibling_next->content[1] == 0, 0, 1)));
 	return (res);
-}*/
+}
 
 static int	nonbrace(t_param *param, t_treenode *node, t_control *ctrl)
 {
@@ -78,6 +95,7 @@ static int	exec_rec(t_param *param, t_treenode *node, t_control *ctrl)
 int	exec_text_tree(t_param *param)
 {
 	int			res;
+	t_control	ctrl;
 
 	if (!param || !param->text_tree || !param->text_tree->root
 		|| !param->text_tree->root->child)
@@ -87,6 +105,9 @@ int	exec_text_tree(t_param *param)
 	}
 	if (param->opts.debug_output_level & DBG_PRINT_TREE_BEFORE_EXEC)
 		ft_tree_print_s(param->text_tree);
-	res = exec_rec(param, param->text_tree->root->child, 0);
+	ctrl = make_control_set_redirs(param->text_tree->root->child);
+	if (!ctrl)
+		return (2);
+	res = exec_rec(param, param->text_tree->root->child, ctrl);
 	return (res);
 }
