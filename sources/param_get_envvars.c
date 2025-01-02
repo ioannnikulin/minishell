ca04b0d9ed6e1c25323b65dcf2f5fb5b1e732d99
Tatiana Retiukhina <taretiuk@student.42berlin.de>
2025-01-02 16:07:31 +0100

    dynamic memory reduced

--- sources/param_get_envvars.c
+++ sources/param_get_envvars.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 20:45:38 by inikulin          #+#    #+#             */
-/*   Updated: 2024/12/24 15:52:08 by taretiuk         ###   ########.fr       */
+/*   Updated: 2025/01/02 16:08:49 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,91 +15,69 @@
 static int	calc_len(const char *s, int *start, int *len_1, int *len_2)
 {
 	int	i;
-	int	delim;
 
 	i = 0;
 	*len_1 = 0;
 	*len_2 = 0;
-	delim = 0;
 	while (s[i])
 	{
-		if (s[i] == '=' && delim == 0)
+		if (s[i] == '=')
 		{
-			delim = 1;
-			*len_1 = i;
+			*len_1 = i + 1;
 			*start = i + 1;
+			break ;
 		}
 		i++;
 	}
-	if (delim)
-		*len_2 = i - *len_1 - 1;
-	else
-		*len_2 = 0;
+	while (s[i])
+		i++;
+	*len_2 = i - *start;
 	return (0);
 }
 
-char	**split_env(char *s)
+void	split_env(char *s, char *key, char *value)
 {
-	int		key_start;
-	int		len_1;
-	int		len_2;
-	char	**res;
+	int			val_start;
+	int			key_len;
+	int			val_len;
 
-	calc_len(s, &key_start, &len_1, &len_2);
-	res = (char **)ft_calloc_if(sizeof(char *) * 3, 1);
-	if (!res)
-		return (NULL);
-	res[0] = ft_substr(s, 0, len_1);
-	if (!res[0])
-	{
-		free(res);
-		return (NULL);
-	}
-	res[1] = ft_substr(s, key_start, len_2);
-	if (!res[1])
-	{
-		ft_free_ss_uptonull((void **)res);
-		return (NULL);
-	}
-	res[2] = NULL;
-	return (res);
+	calc_len(s, &val_start, &key_len, &val_len);
+	ft_strlcpy(key, s, key_len);
+	ft_strcpy(value, s + val_start);
 }
 
 static int	extract_path_env(t_param *param, char *s)
 {
-	int		sz;
-	int		i;
-	char	**tmp;
+	char	*start;
+	char	*current;
 
-	sz = 0;
-	i = 0;
-	tmp = NULL;
-	tmp = ft_split(s, ':', &sz);
-	if (!tmp)
-		return (1);
-	while (tmp[i])
+	start = s;
+	while (*start)
 	{
-		if (!ft_dlist_add_back_s(&param->envvar_path_head, tmp[i]))
+		current = start;
+		while (*start && *start != ':')
+			start++;
+		if (*start == ':')
 		{
-			ft_free_ss_uptonull((void **)tmp);
-			return (1);
+			*start = '\0';
+			start++;
 		}
-		i++;
+		if (!ft_dlist_add_back_s(&param->envvar_path_head, current))
+			return (1);
 	}
-	ft_free_ss_uptonull((void **)tmp);
 	return (0);
 }
 
-static int	handle_envvar(t_param *param, char **s)
+static int	handle_envvar(t_param *param, char *key, char *value)
 {
-	if (ft_strncmp(s[0], "PATH", 4) == 0)
+	if (ft_strncmp(key, "PATH", 5) == 0)
 	{
-		if (extract_path_env(param, s[1]))
+		if (extract_path_env(param, value))
 			return (1);
 	}
-	else if (ft_strncmp(s[0], "PWD", 3) == 0)
+	else if (ft_strncmp(key, "PWD", 4) == 0)
 		return (0);
-	else if (ft_mapss_add(param->envvars, s[0], s[1]))
+	else if (ft_mapss_add(param->envvars, key, value))
 		return (1);
 	return (0);
 }
@@ -108,25 +86,16 @@ static int	handle_envvar(t_param *param, char **s)
 int	param_get_envvars(t_param *param, char **envp)
 {
 	int		i;
-	char	**tmp;
+	char	key[256];
+	char	val[256];
 
 	i = 0;
-	tmp = NULL;
 	while (envp[i])
 	{
-		tmp = split_env(envp[i]);
-		if (!tmp || !tmp[0])
-		{
-			ft_free_ss_uptonull((void **)tmp);
+		split_env(envp[i], key, val);
+		if (handle_envvar(param, key, val))
 			return (1);
-		}
-		if (handle_envvar(param, tmp))
-		{
-			ft_free_ss_uptonull((void **)tmp);
-			return (1);
-		}
 		i++;
-		ft_free_ss_uptonull((void **)tmp);
 	}
 	return (0);
 }
